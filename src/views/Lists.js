f56463c1f5d781aa2d83493c3ce2eec4e456c907
Nathan Keith <nashvegasnate@gmail.com>
2021-06-10 12:19:56 -0500

    finished basic view setup

--- src/views/Lists.js
+++ src/views/Lists.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+// import PropTypes from 'prop-types';
+import { Button } from 'reactstrap';
 
-export default function Lists() {
+function Lists() {
+  const [showButton, setShowButton] = useState(false);
+  const handleClick = () => {
+    setShowButton((prevState) => !prevState);
+  };
+
   return (
-    <div>
+   <>
+    <section className="header mt-2">
+      { !showButton
+        ? <Button className="m-2 btn-lg" color='danger' onClick={handleClick}>Add A List</Button>
+        : <div>
+        <Button className="m-2 btn-lg" color='info' onClick={handleClick}>Close</Button>
+      </div>
+      }
+    </section>
       <h1>This is the Lists page</h1>
-    </div>
+    </>
   );
 }
+
+// Lists.propTypes = {
+//   user: PropTypes.any
+// };
+
+export default Lists;
--- src/views/Songs.js
+++ src/views/Songs.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button } from 'reactstrap';
 
 export default function Songs() {
+  const [showButton, setShowButton] = useState(false);
+  const handleClick = () => {
+    setShowButton((prevState) => !prevState);
+  };
   return (
-    <div>
+    <>
+    <section className="header mt-2">
+      { !showButton
+        ? <Button className="m-2 btn-lg" color='danger' onClick={handleClick}>Add Song</Button>
+        : <div>
+        <Button className="m-2 btn-lg" color='info' onClick={handleClick}>Close</Button>
+        </div>
+      }
+    </section>
       <h1>This is the Songs page</h1>
-    </div>
+    </>
   );
 }
