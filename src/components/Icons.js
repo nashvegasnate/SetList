16a6aaff5d1601fc501d8c5d9a551de7f46de76b
Nathan Keith <nashvegasnate@gmail.com>
2021-07-06 14:02:19 -0500

    added color to landing page

---
+++ src/components/Icons.js
@@ -0,0 +1,7 @@
+import { IconContext } from "react-icons";
+
+<IconContext.Provider value={{ color: "blue", className: "global-class-name" }}>
+  <div>
+    <FaFolder />
+  </div>
+</IconContext.Provider>
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import firebase from 'firebase/app';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/index.scss';
 import App from './App';
 import firebaseConfig from './helpers/apiKeys';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/index.scss';
 
 firebase.initializeApp(firebaseConfig);
 
